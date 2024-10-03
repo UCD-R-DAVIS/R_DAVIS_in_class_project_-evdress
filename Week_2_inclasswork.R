80b83a6db86f1c5d4c5060e9ec4e11a2d5afce08
Eloise Dresser <evdresser@ucdavis.edu>
2024-10-03 14:00:48 -0700

    week 2 in class work (●'◡'●)

---
+++ Week_2_inclasswork.R
@@ -0,0 +1,30 @@
+exp(3)
+log(2, 4)
+log(base=4, x=2)
+log(2 , 4)
+#nested functions
+sqrt(exp(4))
+
+#comparisons
+mynumber <- 6
+mynumber ==5
+mynumber != 5
+# !=
+
+othernumber <- -3
+othernumber * mynumber
+
+#object name conventions
+numSamples <- 50
+# rm(x) removes things
+
+#challenge
+elephant1_kg<- 3492
+elephant1_lb <- 7757
+
+elephant2_lb <- elephant1_kg * 2.2
+elephant2_lb
+rm(elephant1_lb)
+
+myelephants <- c(elephant2_lb, elephant_lb)
+which(myelephants == max(myelephants))
